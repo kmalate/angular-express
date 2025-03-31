e8505e2f8aee5836092d9a28baa7be34738019f5
Khalid Malate <khalid.malate@gmail.com>
2025-03-31 09:46:12 -0400

    - installed bootstrap - add styling - navigation

--- my-angular-app/src/app/protected/protected.component.ts
+++ my-angular-app/src/app/protected/protected.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-protected',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './protected.component.html',
   styleUrl: './protected.component.css'
 })
 export class ProtectedComponent implements OnInit {
   message: String = "";
+  isSuccess = false;
 
   constructor(private userService: UserService) {}
   ngOnInit(): void {
@@ -16,6 +18,7 @@ export class ProtectedComponent implements OnInit {
       next : (response) => {
         if (response) {
           this.message = "You are authenticated!";
+          this.isSuccess = true;
         }
       },
       error: (error) => {
